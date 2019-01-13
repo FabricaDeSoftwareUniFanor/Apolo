bcc92a75a244de19a49b07d9d8ab4f0c405bf3fb
Igor Silva <igorlucas.ti@outlook.com>
2019-01-12 23:47:36 -0300

    iniciando o projeto com o web-app

---
+++ web-app/js/controllers/homeCtrl.js
@@ -0,0 +1,18 @@
+angular.module("Apolo").controller('homeCtrl',homeCtrl);
+
+function homeCtrl(){
+    / * jshint validthis: true * /
+    var vm = this;
+    vm.name = "Projeto Apolo";
+    vm.description = "Aplicativo para avaliação física";
+    vm.anything = anything; 
+
+    function anything(){
+
+    }
+}
+
+
+
+
+
